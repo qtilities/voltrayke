28d2acc601e9ff79ed1042eb6bb0ede4935df7cb
redtide <redtid3@gmail.com>
2021-11-18 16:40:49 +0100

    Updated resources

--- resources/translations/voltrayke_it_IT.ts
+++ resources/translations/voltrayke_it_IT.ts
@@ -2,64 +2,79 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="it_IT">
 <context>
+    <name>DialogAbout</name>
+    <message>
+        <location filename="../../src/ui/dialogabout.ui" line="76"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogabout.h" line="76"/>
+        <source>&amp;Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogabout.cpp" line="43"/>
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>DialogPrefs</name>
     <message>
         <location filename="../../src/ui/dialogprefs.ui" line="20"/>
-        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="238"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="247"/>
+        <location filename="../../src/ui/dialogprefs.cpp" line="34"/>
         <source>Preferences</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/ui/dialogprefs.ui" line="58"/>
-        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="242"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="251"/>
         <source>Device</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/ui/dialogprefs.ui" line="73"/>
-        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="239"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="248"/>
         <source>Audio Device</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/ui/dialogprefs.ui" line="103"/>
-        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="240"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="249"/>
         <source>Card:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../../src/ui/dialogprefs.ui" line="110"/>
-        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="241"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="250"/>
         <source>Channel:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/ui/dialogprefs.ui" line="145"/>
-        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="247"/>
+        <location filename="../../src/ui/dialogprefs.ui" line="152"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="256"/>
         <source>Behaviour</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/ui/dialogprefs.ui" line="166"/>
-        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="243"/>
+        <location filename="../../src/ui/dialogprefs.ui" line="173"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="252"/>
         <source>Volume Control Command</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/ui/dialogprefs.ui" line="209"/>
-        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="244"/>
+        <location filename="../../src/ui/dialogprefs.ui" line="216"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="253"/>
         <source>Volume Scrolling</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/ui/dialogprefs.ui" line="245"/>
-        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="245"/>
+        <location filename="../../src/ui/dialogprefs.ui" line="252"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="254"/>
         <source>Scroll Step:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/ui/dialogprefs.ui" line="258"/>
-        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="246"/>
+        <location filename="../../src/ui/dialogprefs.ui" line="265"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/VolTrayke_autogen/include/ui_dialogprefs.h" line="255"/>
         <source>Fine Scroll Step:</source>
         <translation type="unfinished"></translation>
     </message>
@@ -67,53 +82,36 @@
 <context>
     <name>MenuActions</name>
     <message>
-        <location filename="../../src/ui/menuactions.cpp" line="16"/>
+        <location filename="../../src/ui/menuactions.cpp" line="80"/>
         <source>Auto&amp;start</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/ui/menuactions.cpp" line="17"/>
+        <location filename="../../src/ui/menuactions.cpp" line="84"/>
         <source>&amp;About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/ui/menuactions.cpp" line="18"/>
+        <location filename="../../src/ui/menuactions.cpp" line="85"/>
         <source>&amp;Preferences</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/ui/menuactions.cpp" line="19"/>
+        <location filename="../../src/ui/menuactions.cpp" line="86"/>
         <source>&amp;Quit</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../src/ui/menuactions.cpp" line="43"/>
-        <source>Error loading about file</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../src/ui/menuactions.cpp" line="52"/>
-        <source>About</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>MenuVolume</name>
     <message>
-        <location filename="../../src/ui/menuvolume.ui" line="47"/>
-        <location filename="../../src/ui/menuvolume.cpp" line="45"/>
+        <location filename="../../src/ui/menuvolume.cpp" line="48"/>
         <source>Mixer</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../src/ui/menuvolume.ui" line="60"/>
-        <location filename="../../src/ui/menuvolume.cpp" line="32"/>
+        <location filename="../../src/ui/menuvolume.cpp" line="37"/>
         <source>Mute</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../src/ui/menuvolume.cpp" line="31"/>
-        <source>0</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
