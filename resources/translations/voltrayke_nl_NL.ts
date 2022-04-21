546daedda0095578514e6875702e067330978da0
Heimen Stoffels <vistausss@fastmail.com>
2022-04-21 14:39:16 +0200

    Added Dutch translation

---
+++ resources/translations/voltrayke_nl_NL.ts
@@ -0,0 +1,160 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl_NL">
+<context>
+    <name>DialogAbout</name>
+    <message>
+        <location filename="../../src/ui/dialogabout.ui" line="14"/>
+        <location filename="ui_dialogabout.h" line="71"/>
+        <source>About</source>
+        <translation>Over</translation>
+    </message>
+</context>
+<context>
+    <name>DialogPrefs</name>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="20"/>
+        <location filename="ui_dialogprefs.h" line="265"/>
+        <source>Preferences</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="49"/>
+        <location filename="ui_dialogprefs.h" line="271"/>
+        <source>Device</source>
+        <translation>Apparaat</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="67"/>
+        <location filename="ui_dialogprefs.h" line="266"/>
+        <source>Audio device</source>
+        <translation>Audio-apparaat</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="73"/>
+        <location filename="ui_dialogprefs.h" line="267"/>
+        <source>Backend:</source>
+        <translation>Backend:</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="83"/>
+        <location filename="ui_dialogprefs.h" line="268"/>
+        <source>Channel:</source>
+        <translation>Kanaal:</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="93"/>
+        <location filename="ui_dialogprefs.h" line="269"/>
+        <source>Normalize volume</source>
+        <translation>Volumeniveau normaliseren</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="100"/>
+        <location filename="ui_dialogprefs.h" line="270"/>
+        <source>Allow volume beyond 100% (0dB)</source>
+        <translation>Volumeniveau boven 100% toestaan (0dB)</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="111"/>
+        <location filename="ui_dialogprefs.h" line="279"/>
+        <source>Behaviour</source>
+        <translation>Gedrag</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="138"/>
+        <location filename="ui_dialogprefs.h" line="272"/>
+        <source>External mixer command</source>
+        <translation>Extern mengpaneel (opdracht)</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="168"/>
+        <location filename="ui_dialogprefs.h" line="273"/>
+        <source>Volume scrolling</source>
+        <translation>Volumescrollen</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="198"/>
+        <location filename="ui_dialogprefs.h" line="274"/>
+        <source>Scroll step:</source>
+        <translation>Scrollstap:</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="211"/>
+        <location filename="ui_dialogprefs.h" line="275"/>
+        <source>Page step:</source>
+        <translation>Paginastap:</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="241"/>
+        <location filename="ui_dialogprefs.h" line="276"/>
+        <source>Mouse</source>
+        <translation>Muis</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="247"/>
+        <location filename="ui_dialogprefs.h" line="277"/>
+        <source>Show on mouse click</source>
+        <translation>Tonen na aanklikken</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="254"/>
+        <location filename="ui_dialogprefs.h" line="278"/>
+        <source>Mute on middle click</source>
+        <translation>Dempen met middelste muisknop</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="278"/>
+        <location filename="ui_dialogprefs.h" line="282"/>
+        <source>Notifications</source>
+        <translation>Meldingen</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="284"/>
+        <location filename="ui_dialogprefs.h" line="280"/>
+        <source>Always notify about volume changes</source>
+        <translation>Melding tonen bij volume-aanpassingen</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/dialogprefs.ui" line="291"/>
+        <location filename="ui_dialogprefs.h" line="281"/>
+        <source>Notify about volume changes with keyboard</source>
+        <translation>Melding tonen bij volume-aanpassingen met toetsenbord</translation>
+    </message>
+</context>
+<context>
+    <name>azd::MenuVolume</name>
+    <message>
+        <location filename="../../src/ui/menuvolume.cpp" line="38"/>
+        <source>Mute</source>
+        <translation>Dempen</translation>
+    </message>
+    <message>
+        <location filename="../../src/ui/menuvolume.cpp" line="54"/>
+        <source>Mixer</source>
+        <translation>Mengpaneel</translation>
+    </message>
+</context>
+<context>
+    <name>azd::VolTrayke</name>
+    <message>
+        <location filename="../../src/application.cpp" line="86"/>
+        <source>Auto&amp;start</source>
+        <translation>Automatisch op&amp;starten</translation>
+    </message>
+    <message>
+        <location filename="../../src/application.cpp" line="116"/>
+        <source>&amp;About</source>
+        <translation>&amp;Over</translation>
+    </message>
+    <message>
+        <location filename="../../src/application.cpp" line="117"/>
+        <source>&amp;Preferences</source>
+        <translation>&amp;Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../../src/application.cpp" line="118"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Afsluiten</translation>
+    </message>
+</context>
+</TS>
