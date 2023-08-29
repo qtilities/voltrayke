45d0c4793655c89831892975d632580cf67e87c1
JungHee Lee <36289819+MarongHappy@users.noreply.github.com>
2023-08-29 14:18:53 +0900

    Create voltrayke_ko.ts

---
+++ resources/translations/voltrayke_ko.ts
@@ -0,0 +1,176 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko">
+<context>
+    <name>Qtilities::Application</name>
+    <message>
+        <location filename="../../src/application.cpp" line="49"/>
+        <source>Auto&amp;start</source>
+        <translation type="unfinished">자동시작(&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="../../src/application.cpp" line="79"/>
+        <source>&amp;About</source>
+        <translation type="unfinished">정보(&amp;A)</translation>
+    </message>
+    <message>
+        <location filename="../../src/application.cpp" line="80"/>
+        <source>&amp;Preferences</source>
+        <translation type="unfinished">환경설정(&amp;P)</translation>
+    </message>
+    <message>
+        <location filename="../../src/application.cpp" line="81"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished">종료(&amp;Q)</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::DialogAbout</name>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="27"/>
+        <source>Information</source>
+        <translation type="unfinished">정보</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="62"/>
+        <source>qrc:/about.html</source>
+        <translation type="unfinished">qrc:/about.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="81"/>
+        <source>Thanks</source>
+        <translation type="unfinished">감사한 분들</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="119"/>
+        <source>qrc:/thanks.html</source>
+        <translation type="unfinished">qrc:/thanks.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="138"/>
+        <source>License</source>
+        <translation type="unfinished">라이선스</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.ui" line="176"/>
+        <source>qrc:/license.html</source>
+        <translation type="unfinished">qrc:/license.html</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="55"/>
+        <source>Author</source>
+        <translation type="unfinished">작성자</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogabout.cpp" line="60"/>
+        <source>About</source>
+        <translation type="unfinished">정보</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::DialogPrefs</name>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="20"/>
+        <source>Preferences</source>
+        <translation type="unfinished">환경설정</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="49"/>
+        <source>Device</source>
+        <translation type="unfinished">장치</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="67"/>
+        <source>Audio device</source>
+        <translation type="unfinished">오디오 장치</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="73"/>
+        <source>Backend:</source>
+        <translation type="unfinished">백엔드:</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="83"/>
+        <source>Channel:</source>
+        <translation type="unfinished">채널:</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="93"/>
+        <source>Normalize volume</source>
+        <translation type="unfinished">노멀라이즈 볼륨</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="100"/>
+        <source>Allow volume beyond 100% (0dB)</source>
+        <translation type="unfinished">100% (0dB) 초과하는 볼륨 허용하기</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="111"/>
+        <source>Behaviour</source>
+        <translation type="unfinished">동작</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="138"/>
+        <source>External mixer command</source>
+        <translation type="unfinished">외부 믹서 명령</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="168"/>
+        <source>Volume scrolling</source>
+        <translation type="unfinished">볼륨 스크롤링</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="198"/>
+        <source>Scroll step:</source>
+        <translation type="unfinished">스크롤 단계:</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="211"/>
+        <source>Page step:</source>
+        <translation type="unfinished">페이지 단계:</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="241"/>
+        <source>Mouse</source>
+        <translation type="unfinished">마우스</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="247"/>
+        <source>Show on mouse click</source>
+        <translation type="unfinished">마우스 클릭 시 표시</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="254"/>
+        <source>Mute on middle click</source>
+        <translation type="unfinished">가운데 클릭 시 음소거</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="278"/>
+        <source>Notifications</source>
+        <translation type="unfinished">알림</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="284"/>
+        <source>Always notify about volume changes</source>
+        <translation type="unfinished">볼륨 변경에 대해 항상 알림</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogprefs.ui" line="291"/>
+        <source>Notify about volume changes with keyboard</source>
+        <translation type="unfinished">키보드를 통해 볼륨 변경 알림</translation>
+    </message>
+</context>
+<context>
+    <name>Qtilities::MenuVolume</name>
+    <message>
+        <location filename="../../src/menuvolume.cpp" line="38"/>
+        <source>Mute</source>
+        <translation type="unfinished">음소거</translation>
+    </message>
+    <message>
+        <location filename="../../src/menuvolume.cpp" line="54"/>
+        <source>Mixer</source>
+        <translation type="unfinished">믹서</translation>
+    </message>
+</context>
+</TS>
